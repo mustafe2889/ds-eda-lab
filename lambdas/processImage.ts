4723569f174a02c2d923928851b20872c03228d2
mustafe2889 <20088394@mail.wit.ie>
2024-11-26 15:37:04 +0000

    Signup resource

---
+++ lambdas/processImage.ts
@@ -0,0 +1,36 @@
+import { SQSHandler } from "aws-lambda";
+import {
+    GetObjectCommand,
+    GetObjectCommandInput,
+    S3Client,
+} from "@aws-sdk/client-s3";
+
+const s3 = new S3Client();
+
+export const handler: SQSHandler = async (event) => {
+    console.log("Event ", JSON.stringify(event));
+    for (const record of event.Records) {
+        const recordBody = JSON.parse(record.body);
+        if (recordBody.Records) {
+            console.log("Record body ", JSON.stringify(recordBody));
+            for (const messageRecord of recordBody.Records) {
+                const s3e = messageRecord.s3;
+                const srcBucket = s3e.bucket.name;
+                const srcKey = decodeURIComponent(
+                    s3e.object.key.replace(/\+/g, " ")
+                );
+                let origimage = null;
+                try {
+                    const params: GetObjectCommandInput = {
+                        Bucket: srcBucket,
+                        Key: srcKey,
+                    };
+                    origimage = await s3.send(new GetObjectCommand(params));
+                    // Process the image...
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }
+    }
+};
--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -5,25 +5,54 @@ import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3n from "aws-cdk-lib/aws-s3-notifications";
 import * as events from "aws-cdk-lib/aws-lambda-event-sources";
 import * as sqs from "aws-cdk-lib/aws-sqs";
-import * as sns from "aws-cdk-lib/aws-sns";
-import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
-import * as iam from "aws-cdk-lib/aws-iam";
-
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EDAAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // S3 Bucket
     const imagesBucket = new s3.Bucket(this, "images", {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       publicReadAccess: false,
     });
 
+    // SQS Queue
+    const queue = new sqs.Queue(this, "img-created-queue", {
+      receiveMessageWaitTime: cdk.Duration.seconds(5),
+    });
+
+    // Lambda Function
+    const processImageFn = new lambdanode.NodejsFunction(
+      this,
+      "ProcessImageFn",
+      {
+        runtime: lambda.Runtime.NODEJS_18_X,
+        entry: `${__dirname}/../lambdas/processImage.ts`,
+        timeout: cdk.Duration.seconds(15),
+        memorySize: 128,
+      }
+    );
+
+    // S3 -> SQS
+    imagesBucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.SqsDestination(queue)
+    );
+
+    // SQS -> Lambda
+    const newImageEventSource = new events.SqsEventSource(queue, {
+      batchSize: 5,
+      maxBatchingWindow: cdk.Duration.seconds(5),
+    });
+
+    processImageFn.addEventSource(newImageEventSource);
+
+    // Permissions
+    imagesBucket.grantRead(processImageFn);
+
     // Output
-    
     new cdk.CfnOutput(this, "bucketName", {
       value: imagesBucket.bucketName,
     });
